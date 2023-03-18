dafda414ad1035e88a1af44d1a2126f4e03f1a53
Skelkingr <aymeric.goupil@gmail.com>
2023-03-18 13:33:32 +0100

    Update MathHelper.h : Add VectorSubstract function.

--- Raytracing/MathHelper.h
+++ Raytracing/MathHelper.h
@@ -13,4 +13,11 @@ float dotProduct(Vector3D u, Vector3D v)
 	return u.x * v.x + u.y * v.y + u.z * v.z;
 }
 
+Vector3D vectorSubstract(Vector3D v, Vector3D u)
+{
+	Vector3D result = { .x = v.x - u.x, .y = v.y - u.y, .z = v.z - u.z };
+
+	return result;
+}
+
 #endif 
