62dba2c2aca21378972354d465966bf54ba3deaa
Skelkingr <aymeric.goupil@gmail.com>
2023-03-18 21:35:33 +0100

    Draw three spheres.

--- Raytracing/Util.h
+++ Raytracing/Util.h
@@ -1,69 +1,157 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <float.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <Windows.h>
 
+#include "MathHelper.h"
+
 const wchar_t CLASS_NAME[] = L"SAMPLE RAYTRACING";
 
-const unsigned int WINDOW_SIZE_X = 600;
-const unsigned int WINDOW_SIZE_Y = 600;
+const unsigned int PROJECTION_PLANE_D = 1;
+
+const unsigned int VIEWPORT_WIDTH = 1;
+const unsigned int VIEWPORT_HEIGHT = 1;
+
+const int WINDOW_WIDTH = 600;
+const int WINDOW_HEIGHT = 600;
+
+struct {
+	int width;
+	int height;
+	uint32_t* pixels;
+} canvas = { 0 };
+
+typedef struct Color
+{
+	int r;
+	int g;
+	int b;
+} Color;
 
 typedef struct Sphere
 {
-	int center[3];
+	Vector3D center;
 	int radius;
-	int color[3];
+	Color color;
 } Sphere;
 
-void DrawSphere(HWND hwnd, RECT rect, Sphere s)
+const Color BACKGROUND_COLOR = { 255, 255, 255 };
+
+const Sphere scene[3] = {
+	{.center = {.x = 0.0f, .y = -1.0f, .z = 3.0f}, .radius = 1, .color = {.r = 255, .g = 0, .b = 0} },
+	{.center = {.x = 2.0f, .y = 0.0f, .z = 4.0f}, .radius = 1, .color = {.r = 0, .g = 0, .b = 255} },
+	{.center = {.x = -2.0f, .y = 0.0f, .z = 4.0f}, .radius = 1, .color = {.r = 0, .g = 255, .b = 0} }
+};
+
+void Render();
+Vector3D CanvasToViewport(int x, int y);
+Color TraceRay(Vector3D O, Vector3D D, float t_min, float t_max);
+Array2D IntersectRaySphere(Vector3D O, Vector3D D, Sphere sphere);
+bool SphereIsNull(Sphere sphere);
+
+void PutPixel(int x, int y, Color color)
 {
-	PAINTSTRUCT ps;
+	x = (canvas.width / 2) + x;
+	y = (canvas.height / 2) + y + 1;
 
-	HBRUSH hBrush = CreateSolidBrush(
-		RGB(
-			s.color[0],
-			s.color[1],
-			s.color[2]
-		)
-	);
-	HPEN hPen = CreatePen(PS_NULL, 0, NULL);
+	if (x < 0 || x >= canvas.width || y < 0 || y >= canvas.height)
+		return;
 
-	BeginPaint(hwnd, &ps);
-	
-	SelectObject(ps.hdc, hBrush);
-	SelectObject(ps.hdc, hPen);
-
-	Ellipse(
-		ps.hdc,
-		s.radius + 10,
-		s.radius + 10,
-		rect.right - 10,
-		rect.bottom - 10
-	);
-
-	EndPaint(hwnd, &ps);
+	canvas.pixels[(y * canvas.width) + x] = RGB(color.b, color.g, color.r);
 }
 
-void PutPixel(HWND hwnd, RECT rect, int x, int y, COLORREF color)
+void Render()
 {
-	PAINTSTRUCT ps;
+	Vector3D O = { 0.0f, 0.0f, 0.0f };
 
-	HBRUSH hBrush = CreateSolidBrush(color);
-	HPEN hPen = CreatePen(PS_NULL, 0, NULL);
+	for (int x = -canvas.width / 2; x < canvas.width / 2; x++)
+	{
+		for (int y = -canvas.height / 2; y < canvas.height / 2; y++)
+		{
+			Vector3D D = CanvasToViewport(x, y);
+			Color color = TraceRay(O, D, 1, FLT_MAX);
+			PutPixel(x, y, color);
+		}
+	}
+}
 
-	BeginPaint(hwnd, &ps);
+Vector3D CanvasToViewport(int x, int y)
+{
+	Vector3D result = {
+		x * (float)VIEWPORT_WIDTH / (float)WINDOW_WIDTH,
+		y * (float)VIEWPORT_HEIGHT / (float)WINDOW_HEIGHT,
+		(float)PROJECTION_PLANE_D
+	};
 	
-	SelectObject(ps.hdc, hBrush);
-	SelectObject(ps.hdc, hPen);
-	
-	Rectangle(
-		ps.hdc,
-		x, y,
-		x + 1, y + 1
-	);
+	return result;
+}
 
-	EndPaint(hwnd, &ps);
+Color TraceRay(Vector3D O, Vector3D D, float t_min, float t_max)
+{
+	float closest_t = FLT_MAX;
+	Sphere closestSphere = { 0 };
+
+	for (int i = 0; i < 3; i++)
+	{
+		Array2D t = IntersectRaySphere(O, D, scene[i]);
+
+		if ((t.a <= t_max && t.a >= t_min) && t.a < closest_t)
+		{
+			closest_t = t.a;
+			closestSphere = scene[i];
+		}
+
+		if ((t.b <= t_max && t.b >= t_min) && t.b < closest_t)
+		{
+			closest_t = t.b;
+			closestSphere = scene[i];
+		}
+	}
+
+	if (SphereIsNull(closestSphere))
+		return BACKGROUND_COLOR;
+
+	return closestSphere.color;
+}
+
+Array2D IntersectRaySphere(Vector3D O, Vector3D D, Sphere sphere)
+{
+	int r = sphere.radius;
+	Vector3D CO = VectorSubstract(O, sphere.center);
+
+	float a = DotProduct(D, D);
+	float b = 2 * DotProduct(CO, D);
+	float c = DotProduct(CO, CO) - r * r;
+
+	float discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0)
+	{
+		Array2D result = { FLT_MAX, FLT_MAX };
+		return result;
+	}
+
+	float t1 = (-b + sqrt(discriminant)) / (2 * a);
+	float t2 = (-b - sqrt(discriminant)) / (2 * a);
+
+	Array2D result = { t1, t2 };
+	return result;
+}
+
+bool SphereIsNull(Sphere sphere)
+{
+	bool centerIsNull = sphere.center.x == 0.0f && sphere.center.y == 0.0f && sphere.center.z == 0.0f;
+	bool colorIsNull = sphere.color.r == 0 && sphere.color.g == 0 && sphere.color.b == 0;
+
+	if (centerIsNull && sphere.radius == 0 && colorIsNull)
+		return true;
+
+	return false;
 }
 
 #endif
--- Raytracing/main.c
+++ Raytracing/main.c
@@ -1,8 +1,13 @@
 #include "Util.h"
 
 #pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+BITMAPINFO canvas_bitmap_info;
+HBITMAP canvas_bitmap;
+HDC canvas_device_context;
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR cmdLine, _In_ int showCmd)
 {
@@ -23,6 +28,12 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
     RegisterClassW(&wc);
 
+    canvas_bitmap_info.bmiHeader.biSize = sizeof(canvas_bitmap_info.bmiHeader);
+    canvas_bitmap_info.bmiHeader.biPlanes = 1;
+    canvas_bitmap_info.bmiHeader.biBitCount = 32;
+    canvas_bitmap_info.bmiHeader.biCompression = BI_RGB;
+    canvas_device_context = CreateCompatibleDC(0);
+
     int posX = (GetSystemMetrics(SM_CXSCREEN) / 2) - (WINDOW_WIDTH / 2);
     int posY = (GetSystemMetrics(SM_CYSCREEN) / 2) - (WINDOW_HEIGHT / 2);
 
@@ -37,6 +48,9 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
         hInstance,
         NULL
     );
+
+    Render(hwnd);
+    UpdateWindow(hwnd);
 
     while (GetMessage(&msg, NULL, 0, 0))
     {
@@ -53,6 +67,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
     case WM_DESTROY:
         PostQuitMessage(0);
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT paint;
+            HDC device_context;
+            device_context = BeginPaint(hwnd, &paint);
+            BitBlt(
+                device_context,
+                paint.rcPaint.left, paint.rcPaint.top,
+                paint.rcPaint.right - paint.rcPaint.left, paint.rcPaint.bottom - paint.rcPaint.top,
+                canvas_device_context,
+                paint.rcPaint.left, paint.rcPaint.top,
+                SRCCOPY
+            );
+            EndPaint(hwnd, &paint);
+        }
+        break;
+    case WM_SIZE:
+        {
+            canvas_bitmap_info.bmiHeader.biWidth = LOWORD(lParam);
+            canvas_bitmap_info.bmiHeader.biHeight = HIWORD(lParam);
+
+            if (canvas_bitmap)
+                DeleteObject(canvas_bitmap);
+        
+            canvas_bitmap = CreateDIBSection(NULL, &canvas_bitmap_info, DIB_RGB_COLORS, &canvas.pixels, 0, 0);
+            SelectObject(canvas_device_context, canvas_bitmap);
+
+            canvas.width = LOWORD(lParam);
+            canvas.height = HIWORD(lParam);
+        }
         break;
     }
 
