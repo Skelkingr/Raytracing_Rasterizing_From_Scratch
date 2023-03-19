40eaf4d100fe108d38074263e7279dca98f34845
Skelkingr <aymeric.goupil@gmail.com>
2023-03-19 13:32:20 +0100

    Update MathHelper.h

--- Raytracing/MathHelper.h
+++ Raytracing/MathHelper.h
@@ -21,6 +21,13 @@ float DotProduct(Vector3D u, Vector3D v)
 	return u.x * v.x + u.y * v.y + u.z * v.z;
 }
 
+Vector3D VectorAdd(Vector3D u, Vector3D v)
+{
+	Vector3D result = { .x = u.x + v.x, .y = u.y + v.y, .z = u.z + v.z };
+
+	return result;
+}
+
 Vector3D VectorSubstract(Vector3D v, Vector3D u)
 {
 	Vector3D result = { .x = v.x - u.x, .y = v.y - u.y, .z = v.z - u.z };
@@ -31,6 +38,13 @@ Vector3D VectorSubstract(Vector3D v, Vector3D u)
 float Length(Vector3D u)
 {
 	return sqrtf(u.x * u.x + u.y * u.y + u.z * u.z);
+}
+
+Vector3D ScalarMul(float scalar, Vector3D u)
+{
+	Vector3D result = { .x = scalar * u.x, .y = scalar * u.y, .z = scalar * u.z };
+
+	return result;
 }
 
 #endif 
